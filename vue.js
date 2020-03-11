88a5e64973bc56a5c3e48c78809fb5ad21226281
Rezo33 <45535800+Rezo33@users.noreply.github.com>
2020-03-11 14:41:50 +0200

    Add files via upload

--- vue.js
+++ vue.js
@@ -5,18 +5,14 @@ let app = new Vue({
 	},
 	methods:{
 		increment:function(){
-			console.log(this.counter)
 			if(this.counter < 3){
 				return this.counter ++
 			}
 			else{
 				 return this.counter = 0;
 			}
-			console.log(this.counter)
 		},
 		decrement:function(){
-			
-			console.log(this.counter)
 			if(this.counter > 0){
 				 return this.counter --
 			}
